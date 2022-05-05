b666548b984eee1b4a53972c49c69143715a0373
zeichi11 <zeichi11@gmail.com>
2022-05-06 01:09:06 +0900

    LocalStorage 사용을 위한 라이브러리 설치(lowdb) - lowdb, lodash 라이브러리 추가 - lowdb 기본 세팅 코드 추가

--- src/TodoApp.vue
+++ src/TodoApp.vue
@@ -6,13 +6,33 @@
 </template>
 
 <script>
-import TodoCreator from './components/Creator.vue'
-import TodoItem from './components/Item.vue'
+import { Low, LocalStorage } from 'lowdb';
+import TodoCreator from './components/Creator.vue';
+import TodoItem from './components/Item.vue';
 
 export default {
   components: {
     TodoCreator,
     TodoItem
+  },
+  data () {
+    return {
+      db: null
+    }
+  },
+  created () {
+    this.initDB();
+  },
+  methods: {
+    initDB() {
+      const adapter = new LocalStorage('todo-app'); // DB, todo-app: dbName
+      this.db = new Low(adapter);
+
+      this.db.data = {
+        todos: [] // Collection
+      }; // write to lowdb
+      this.db.write();
+    }
   }
 }
 </script>
