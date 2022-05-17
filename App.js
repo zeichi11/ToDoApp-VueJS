efa8c622ee59b0be93ddc7558c44d3be9e18733f
zeichi11 <zeichi11@gmail.com>
2022-05-17 22:55:01 +0900

    vue-router 기본세팅 - vue-router 설치 및 기본 파일 세팅 - 컴포넌트 내 아이콘에 대한 설명(title) 리소스 추가 - TodoApp 컴포넌트 구조 변경

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './src/router';
 
 new Vue({
   el: '#app',
+  router,
   render: createElement => createElement(App)
-})
+});
