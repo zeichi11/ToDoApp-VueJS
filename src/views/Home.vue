ef48df94e5816dad1f7942768ad2adbcfc584152
zeichi11 <zeichi11@gmail.com>
2022-05-20 01:05:15 +0900

    vue-router 테스트 페이지 작업 - 테스트 페이지(home, about) 템플릿 작성 - App 화면에 home 버튼 추가 - sylte 일부 수정

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="home">
+    <div>
+        <h1>
+          TodoApp Main
+        </h1>
+        <router-link tag="h2" to="/about">
+          About
+        </router-link>
+        <router-link tag="h2" to="/todos">
+          Todo App
+        </router-link>
+    </div>
+  </section>
+</template>
