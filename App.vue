852e4c42ca1d0588d17b846a9a171e9e34a61388
zeichi11 <zeichi11@gmail.com>
2022-05-21 14:57:45 +0900

    TodoApp 상태 정보 vuex store로 이관 작업 - vuex actions, mutations, getters, state 작성 - 기존 상태 정보 업데이트 함수 이관 - 이관 작업 앱에 반영 - lint 오류 수정

--- App.vue
+++ App.vue
@@ -14,12 +14,11 @@ export default {
     console.log('Router', this.$router);
     console.log('Route', this.$route);
   }
-}
+};
 </script>
 
-
 <style lang="scss">
-  // 현재 페이지가 router-link 태그의 to 속성에 해당하는 페이지인 경우 
+  // 현재 페이지가 router-link 태그의 to 속성에 해당하는 페이지인 경우
   // router-link-exact-active class가 추가된다.
   // 해당 class가 있는 경우 home 화면이므로 home 버튼 메뉴를 보이지 않게 처리한다.
   .to-home.router-link-exact-active {
--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,24 +1,24 @@
 const MSG_CONST = {
-		CREATOR_INPUT_PLACEHOLER: '할 일을 입력하세요!',
-		INVALID_TITLE_ERROR: '유효하지 않은 제목입니다!',
-	},
-	RESOURCES = {
-		EDIT_ITEM: '수정',
-    DELETE_ITEM: '삭제',
-    COMPLETE_EDITING: '완료',
-    CANCEL_EDITING: '취소',
-    ITEM_EDITED: 'edited',
-    ALL_ITEMS: '모든 항목',
-    ACTIVE_ITEMS: '해야 할 항목',
-    COMPLETED_ITEMS: '완료된 항목',
-    SELECT_ALL: '모든 항목 선택',
-    CLEAR_COMPLETED_ITEMS: '완료된 항목 삭제'
-	},
-	DEFAULTS = {
-		DATE_FORM: 'YYYY년 MM월 DD일',
-		ITEM_FILTER_ALL: 'all',
-		ITEM_FILTER_ACTIVE: 'active',
-		ITEM_FILTER_COMPLETED: 'completed'
-	};
+  CREATOR_INPUT_PLACEHOLER: '할 일을 입력하세요!',
+  INVALID_TITLE_ERROR: '유효하지 않은 제목입니다!'
+};
+const RESOURCES = {
+  EDIT_ITEM: '수정',
+  DELETE_ITEM: '삭제',
+  COMPLETE_EDITING: '완료',
+  CANCEL_EDITING: '취소',
+  ITEM_EDITED: 'edited',
+  ALL_ITEMS: '모든 항목',
+  ACTIVE_ITEMS: '해야 할 항목',
+  COMPLETED_ITEMS: '완료된 항목',
+  SELECT_ALL: '모든 항목 선택',
+  CLEAR_COMPLETED_ITEMS: '완료된 항목 삭제'
+};
+const DEFAULTS = {
+  DATE_FORM: 'YYYY년 MM월 DD일',
+  ITEM_FILTER_ALL: 'all',
+  ITEM_FILTER_ACTIVE: 'active',
+  ITEM_FILTER_COMPLETED: 'completed'
+};
 
 export { MSG_CONST, RESOURCES, DEFAULTS };
