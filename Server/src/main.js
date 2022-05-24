c90a5223a06343ec10ba7fa1dd96cf7c13fc5da0
zeichi11 <zeichi11@gmail.com>
2022-05-24 22:04:43 +0900

    express 미들웨어 및 기본 라우팅 처리 스터디 - express 미들웨어 패턴 및 기본 라우팅 처리 스터디 및 테스트

--- Server/src/main.js
+++ Server/src/main.js
@@ -1,11 +1,70 @@
 // @ts-check
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 const PORT = 8000
 
 app.use('/', (req, res) => {
   res.send('Hello, express')
+})
+
+app.use(
+  '/test1',
+  (req, res, next) => {
+    console.log('Middleware1-1')
+    setTimeout(() => {
+      next()
+    }, 1000)
+  },
+  (req, res) => {
+    console.log('Middleware1-2')
+    next()
+  }
+
+  // .... more middleware
+)
+
+app.use(
+  '/test2',
+  (req, res, next) => {
+    console.log('Middleware2-1')
+
+    const requestedAt = new Date()
+    req.requestedAt = requestedAt
+
+    setTimeout(() => {
+      next()
+    }, 1000)
+  },
+  (req, res) => {
+    console.log('Middleware2-2')
+    res.rend(`Hello, world! time: ${req.requestedAt}`)
+  }
+)
+
+app.use('/test3', async (req, res, next) => {
+  console.log('Middleware3-1')
+
+  const fileContent = await fs.promises.readFile('README.txt')
+  req.fileContent = fileContent
+
+  next()
+})
+
+app.use('/test3', (req, res) => {
+  console.log('Middlewar3-2')
+  res.send(`File content : ${req.fileContent}`)
+})
+
+// HTTP Method
+
+app.get('/test', (req, res) => {
+  res.send('get')
+})
+
+app.post('/ab+cd', (req, res) => {
+  res.send('post')
 })
 
 app.listen(PORT, () => {
