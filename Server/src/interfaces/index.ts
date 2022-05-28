71cbefb68f45d87b13968bdee2fe89f3d88a39ab
zeichi11 <zeichi11@gmail.com>
2022-05-28 21:22:37 +0900

    typescript 오류 수정 및 router 기본 코드 추가

---
+++ Server/src/interfaces/index.ts
@@ -0,0 +1,3 @@
+export interface configType {
+  readonly port: string;
+}
--- Server/src/routes/Dashboard.ts
+++ Server/src/routes/Dashboard.ts
@@ -1,3 +1,10 @@
-export default {
-  
-}
+import { Router, Request, Response } from 'express'
+
+const router: Router = express.Router()
+
+router.get('/', (req: Request, res: Response) => {
+  res.send('dashboard page')
+});
+
+
+export default router;
