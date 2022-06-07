99d835793dc95a092290b2b101f46f8cf4e2391c
zeichi11 <zeichi11@gmail.com>
2022-06-07 23:28:12 +0900

    서버 config 파일 로딩 처리 - TODO 크로스 플랫폼을 고려하여 라이브러리 사용할 것(dotenv, cross-env)

--- Server/src/util/config.ts
+++ Server/src/util/config.ts
@@ -1,3 +1,40 @@
-export default {
-  
+import path from 'path'
+import fs from 'fs'
+import { configType } from '../interface'
+
+const DEFAULT_CONFIG: configType = {
+  port: '8080'
 }
+
+let _configRoot: string,
+  _configPath: string,
+  _env: string | undefined
+
+/**
+ * init
+ */
+function initialize() {
+  _env = process.env.NODE_ENV
+  _configRoot = path.join(process.cwd(), '../config')
+  _configPath = path.join(_configRoot, `${_env}.config.json`)
+}
+
+/**
+ * load config json
+ * @returns configType
+ */
+function loadConfig(): configType {
+  initialize()
+
+  let configData = DEFAULT_CONFIG;
+
+  if (_configPath) {
+    if (fs.existsSync(_configPath)) {
+      const configFile = fs.readFileSync(_configPath, 'utf8')
+      configData = JSON.parse(configFile)
+    }
+  }
+  return configData
+}
+
+export default loadConfig()
