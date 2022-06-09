3263c539c0d94958e3ca46479f411686ea953cd8
zeichi11 <zeichi11@gmail.com>
2022-06-10 02:03:37 +0900

    서버 실행 스크립트 작성 및 모듈 설치 - dev, prod 모드 별 서버 실행 스크립트 작성 - typescript 실행을 위한 모듈 설치 - tsconfig 내용 수정 및 pm2 실행을 위한 config 파일 설치

--- Server/src/route/user/index.ts
+++ Server/src/route/user/index.ts
@@ -1,56 +1,56 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
-const PORT = 5000
+// const app = express()
+// const PORT = 5000
 
 
-const app = express()
-const userRouter = express.Router()
+// const app = express()
+// const userRouter = express.Router()
 
-userRouter.get('/', (res, req) => {
-  res.send('User list')
-})
+// userRouter.get('/', (res, req) => {
+//   res.send('User list')
+// })
 
-const USERS = {
-  15: {
-    nickname: 'foo'
-  }
-}
+// const USERS = {
+//   15: {
+//     nickname: 'foo'
+//   }
+// }
 
-userRouter.param('id', (req, res, next, value) => {
-  console.log('id parameter', value)
-  req.user = USER[value]
-  next()
-})
+// userRouter.param('id', (req, res, next, value) => {
+//   console.log('id parameter', value)
+//   req.user = USER[value]
+//   next()
+// })
 
-userRouter.get('/:id', (req, res) => {
-  console.log('userRouter get ID')
-  res.send(req.user)
-})
+// userRouter.get('/:id', (req, res) => {
+//   console.log('userRouter get ID')
+//   res.send(req.user)
+// })
 
-userRouter.post('/', (req, res) => {
-  // Register user
-  console.log('User registered.')
-})
+// userRouter.post('/', (req, res) => {
+//   // Register user
+//   console.log('User registered.')
+// })
 
-const bodyParser = require('body-parser')
-const userRouter = express.Router()
-const app = express()
+// const bodyParser = require('body-parser')
+// const userRouter = express.Router()
+// const app = express()
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-userRouter.post('/:id/nickname', (req, res) => {
-  // req.body: {"nickname": "bar"}
-  const { user } = req
-  const { nickname } = req.body
+// userRouter.post('/:id/nickname', (req, res) => {
+//   // req.body: {"nickname": "bar"}
+//   const { user } = req
+//   const { nickname } = req.body
 
-  user.nickname = nickname
+//   user.nickname = nickname
 
-  res.send(`User nickname updated: ${nickname}`)
-})
+//   res.send(`User nickname updated: ${nickname}`)
+// })
 
-app.use('/users', userRouter)
+// app.use('/users', userRouter)
 
-app.listen(PORT, () => {
-  console.log(`The Express server is listening at port: ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`The Express server is listening at port: ${PORT}`)
+// })
--- Server/src/router.ts
+++ Server/src/router.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express'
-import { RouteTableType } from './routes/interface';
+import { RouteTableType } from './route/interface';
 
 export default {
   set (app: Express, routerTable: RouteTableType) {
