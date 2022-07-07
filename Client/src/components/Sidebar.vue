64725f1d9a2a325bda8a7bda1d4860eee014a03f
zeichi11 <zeichi11@gmail.com>
2022-07-08 02:34:39 +0900

    UI 컴포넌트 생성을 위한 메타데이터 객체 정의

--- Client/src/components/Sidebar.vue
+++ Client/src/components/Sidebar.vue
@@ -71,15 +71,61 @@ export default {
         }
       }
     },
+    UiMetaData () {
+      return {
+      }
+    }
     workspaceBtn () {
       return {
+        type: 'iconLabel'
         iconSrc: 'url',
         labelStr: 'Workspace',
-        handleIconClick () {
-          console.log('icon click')
-        },
-        handleLabelClick () {
-          console.log('label click')
+        eventHandling: {
+          type: 'twoButton',
+          handler: [
+            function () {
+              console.log('icon clicked')
+            },
+            function () {
+              console.log('label clicked')
+            }
+          ]
+        }
+      }
+    },
+    membersBtn () {
+      return {
+        type: 'iconLabelAdd',
+        iconSrc: 'url',
+        labelStr: 'Members',
+        eventHandling: {
+          type: 'twoButton',
+          handler: [
+            function () {
+              console.log('icon clicked')
+            },
+            function () {
+              console.log('label clicked')
+            },
+            function () {
+              console.log('add button clicked')
+            }
+          ]
+        }
+      }
+    },
+    settingsBtn () {
+      return {
+        type: 'iconLabel',
+        iconSrc: 'url',
+        labelStr: 'Settings',
+        eventHandling: {
+          type: 'oneButton',
+          handler: [
+            function () {
+              console.log('button clicked')
+            }
+          ]
         }
       }
     }
