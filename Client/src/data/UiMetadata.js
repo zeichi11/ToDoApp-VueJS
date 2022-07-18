83f6dce50d21a0cbdaf4974c22abade508436bf5
zeichi11 <zeichi11@gmail.com>
2022-07-18 23:39:39 +0900

    사이드바 메뉴 작업 중

--- Client/src/data/UiMetadata.js
+++ Client/src/data/UiMetadata.js
@@ -1,55 +1,82 @@
-
-workspaceBtn() {
-  return {
-    type: 'iconLabel'
-    iconSrc: 'url',
-    labelStr: 'Workspace',
-    eventHandling: {
-      type: 'twoButton',
-      handler: [
-        function () {
-          console.log('icon clicked')
+export default {
+  sidebar: {
+    menuContainer: {
+      list: [
+        //workspace button
+        {
+          ui: 'button',
+          type: 'iconLabel',
+          iconSrc: 'url',
+          labelStr: 'Workspace'
         },
-        function () {
-          console.log('label clicked')
-        }
-      ]
-    }
-  }
-},
-membersBtn () {
-  return {
-    type: 'iconLabelAdd',
-    iconSrc: 'url',
-    labelStr: 'Members',
-    eventHandling: {
-      type: 'twoButton',
-      handler: [
-        function () {
-          console.log('icon clicked')
+        // members button
+        {
+          ui: 'button',
+          type: 'iconLabelAdd',
+          iconSrc: 'url',
+          labelStr: 'Members'
         },
-        function () {
-          console.log('label clicked')
-        },
-        function () {
-          console.log('add button clicked')
-        }
-      ]
-    }
-  }
-},
-settingsBtn () {
-  return {
-    type: 'iconLabel',
-    iconSrc: 'url',
-    labelStr: 'Settings',
-    eventHandling: {
-      type: 'oneButton',
-      handler: [
-        function () {
-          console.log('button clicked')
-        }
+        // settings button
+        {
+        type: 'iconLabel',
+        iconSrc: 'url',
+        labelStr: 'Settings'
       ]
     }
   }
 }
+
+// workspaceBtn() {
+//   return {
+//     type: 'iconLabel'
+//     iconSrc: 'url',
+//     labelStr: 'Workspace',
+//     eventHandling: {
+//       type: 'twoButton',
+//       handler: [
+//         function () {
+//           console.log('icon clicked')
+//         },
+//         function () {
+//           console.log('label clicked')
+//         }
+//       ]
+//     }
+//   }
+// },
+// membersBtn () {
+//   return {
+//     type: 'iconLabelAdd',
+//     iconSrc: 'url',
+//     labelStr: 'Members',
+//     eventHandling: {
+//       type: 'twoButton',
+//       handler: [
+//         function () {
+//           console.log('icon clicked')
+//         },
+//         function () {
+//           console.log('label clicked')
+//         },
+//         function () {
+//           console.log('add button clicked')
+//         }
+//       ]
+//     }
+//   }
+// },
+// settingsBtn () {
+//   return {
+//     type: 'iconLabel',
+//     iconSrc: 'url',
+//     labelStr: 'Settings',
+//     eventHandling: {
+//       type: 'oneButton',
+//       handler: [
+//         function () {
+//           console.log('button clicked')
+//         }
+//       ]
+//     }
+//   }
+// }
